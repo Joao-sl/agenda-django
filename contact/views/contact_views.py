ed9e2b32dfba07167dc091edcf6e443d8aa5631a
Joao <joao-victor1982@hotmail.com>
2024-08-23 15:25:59 -0300

    A-471 to A-478

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.core.paginator import Paginator
 from contact.models import Contact
-from django.http import Http404
 # Create your views here.
 
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
 
-    context = {'contacts': contacts, 'site_title': 'Home - '}
+    paginator = Paginator(contacts, 25)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {'page_obj': page_obj, 'site_title': 'Home - '}
 
     return render(request, 'contact/index.html', context)
 
@@ -15,9 +20,31 @@ def index(request):
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
-
     site_title = f'{single_contact.first_name} {single_contact.last_name} - '
-
     context = {'contact': single_contact, 'site_title': site_title}
 
     return render(request, 'contact/contact.html', context)
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(Q(first_name__icontains=search_value) |
+                Q(last_name__icontains=search_value) |
+                Q(phone__icontains=search_value) |
+                Q(email__icontains=search_value)) \
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 25)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {'page_obj': page_obj,
+               'site_title': 'Home - ',
+               'search_value': search_value}
+
+    return render(request, 'contact/index.html', context)
